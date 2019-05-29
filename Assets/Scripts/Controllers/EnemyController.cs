e6b7f3079cc4b10bc5d100e7de9485d060f98da0
Alireza <Alirezag53@gmail.com>
2019-05-29 14:47:01 +0430

    Enemy AI Implemented

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour {
+
+    public float lookRadius = 5f;
+
+    Transform target;
+    NavMeshAgent agent;
+    Animator animator;
+
+    public LayerMask rayMask;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        target = PlayerManager.instance.player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        Vector3 direction = Vector3.Normalize((target.transform.position - new Vector3(0, 0.2f, 0)) - transform.position);
+
+        if(distance <= lookRadius)
+        {
+            FaceTarget();
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit hit;
+            RaycastHit playerHit;
+
+            /*RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray, 2f, 11, queryTriggerInteraction: QueryTriggerInteraction.Ignore);
+
+            for(int i =0; i < hits.Length; i++)
+            {
+                Debug.Log(hits[i].transform.name);
+            }*/
+
+            Debug.DrawRay(transform.position, direction *2, color: Color.red, duration: 1f);
+            //int playerMask = 1 << 9;
+            //int wallMask = 1 << 12;
+            if (Physics.Raycast(ray, out hit, 2, rayMask))
+            {
+                Debug.Log(hit.collider.name);;
+                if (hit.collider.tag == "Player")
+                {
+                    agent.SetDestination(target.position);
+                    animator.SetBool("isRunning", true);
+                    animator.SetBool("isAttacking", false);
+
+                    if (distance <= agent.stoppingDistance)
+                    {
+                        //Face the target
+                        FaceTarget();
+                        animator.SetBool("isAttacking", true);
+
+                        //if Character is in range after attack, Game over.
+                        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AfterAttack"))
+                        {
+                            if (distance <= agent.stoppingDistance)
+                            {
+                                GameOver();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+	}
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    void GameOver()
+    {
+        //Replace 'Game Over' with your game over scene's name.
+        CursorManager.instance.Unlock();
+        Debug.Log("Cursor unlocked");
+        SceneManager.LoadScene("GameOver");
+    }
+}
---
+++ Assets/Scripts/Controllers/FpsMovement.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(CharacterController))]
+
+// basic WASD-style movement control
+public class FpsMovement : MonoBehaviour
+{
+    [SerializeField] private Camera headCam;
+
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float rotationVert = 0;
+
+    private CharacterController charController;
+
+    public Interactable focus;
+
+    public CursorLockMode wantedMode;
+
+
+    void Start()
+    {
+        Cursor.lockState = wantedMode;
+        charController = GetComponent<CharacterController>();
+        Cursor.visible = (CursorLockMode.Locked != wantedMode);
+    }
+
+    void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+        MoveCharacter();
+        RotateCharacter();
+        RotateCamera();
+        Interaction();
+    }
+    private void Interaction()
+    {
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
+
+            RaycastHit hit;
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward, color: Color.white, duration: 2f);
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            if (Physics.Raycast(ray, out hit, 1000, 11))
+            {
+                /*if(hit.collider.tag!="Player"&&hit.collider.tag!="Enemy")
+            {
+                position = hit.point;
+            }*/
+
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+            
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+                else
+                {
+                    Inventory.instance.activeItem = null; //If we didn't hit an item, turn off the using item display
+                    //TODO: This should be decided.
+                }
+
+
+            }
+        }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        // If our focus has changed
+        if (newFocus != focus)
+        {
+            // Defocus the old one
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;   // Set our new focus
+                                //motor.FollowTarget(newFocus);	// Follow the new focus
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    // Remove our current focus
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        //motor.StopFollowingTarget();
+    }
+
+
+    // Update is called once per frame
+
+    private void MoveCharacter()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        charController.Move(movement);
+    }
+
+    private void RotateCharacter()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+    }
+
+    private void RotateCamera()
+    {
+        rotationVert -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        rotationVert = Mathf.Clamp(rotationVert, minimumVert, maximumVert);
+
+        headCam.transform.localEulerAngles = new Vector3(
+            rotationVert, headCam.transform.localEulerAngles.y, 0
+        );
+    }
+}
